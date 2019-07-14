349500f43f9598aa0912251175901d6ce1d83433
pitulujan <lujanmartin91@gmail.com>
2019-07-14 13:36:29 -0300

    take-home

--- logic.py
+++ logic.py
@@ -23,16 +23,18 @@ def clean_asset_tags(x):
 
 pandas_ams_cms_input_precursor = pd.DataFrame(columns=['asset_tag','source_system','source_system_id','source_system_asset_name','product','product_use','cmh_id','device_location_status','last_pinged_at','mac_address','public_ip_address','sku','created_at','installed_date','deleted_at','reason_for_deletion','export_date'])
 
-
-b=ams_sf_assets_input.sort_values(['created_date'],ascending=False).groupby('asset_tag')[['sku']].first().reset_index()
+ams_sf_assets_input.sort_values(['created_date'],ascending=False,inplace=True)
+b=ams_sf_assets_input.groupby('asset_tag')[['sku']].first().reset_index()
 b['asset_tag']=b['asset_tag'].str.upper()
 
-bb= pd.merge(a, b, how='left', left_on=['asset_id'],right_on=['asset_tag'])
 
-a['asset_id'] = a['asset_id'].str.upper()
-s1 = pd.merge(a, b, how='left', left_on=['asset_id'],right_on=['asset_tag'],suffixes=('_a','_b'))
-s2 = pd.merge(s1, c, how='left', left_on=a['custom_rom_version'],right_on=['custom_rom_version'],suffixes=('','_c'))
+mdm_devices_history['asset_id'] = mdm_devices_history['asset_id'].str.upper()
+s1 = pd.merge(mdm_devices_history, b, how='left', left_on=['asset_id'],right_on=['asset_tag'],suffixes=('_a','_b'))
+s2 = pd.merge(s1, shared_mdm_custom_rom_sku_mapping, how='left', left_on=a['custom_rom_version'],right_on=['custom_rom_version'],suffixes=('','_c'))
 s3 = pd.merge(s2, d, how='left', left_on=a['asset_id'],right_on=['asset_id'],suffixes=('','_d'))
+
+b=pd.concat([s3[list(mdm_devices_history.columns)],s3.sku.combine_first(s3.sku_c)],axis=1)
+b['missing_mac_address']=s3['mac_address_d']
 
 
 
