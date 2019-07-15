4cefc26f68b1ae12365feb522a6c7afb51ef762d
pitulujan <lujanmartin91@gmail.com>
2019-07-15 14:18:02 -0300

    take-home

--- logic.py
+++ logic.py
@@ -44,21 +44,27 @@ s1 = pd.merge(broadsign_hosts_history, broadsign_monitor_polls_history, how='lef
 s2 = pd.merge(s1, c, how='left', left_on=[broadsign_hosts_history.primary_mac_address.combine_first(broadsign_hosts_history.secondary_mac_address).str.replace(':','').str.upper().str.strip()],right_on=['mac_address'],suffixes=('','_c'))
 s3 = pd.merge(s2, ams_broadsign_migration_devices_history, how='left', left_on=[broadsign_hosts_history['host_id'],broadsign_hosts_history['export_date']],right_on=[ams_broadsign_migration_devices_history.bs_host_id.astype('int64'),'export_date'],suffixes=('','_d'))
 
-c=pd.concat([s3[['name','display_unit_id','poll_last_utc','host_id','public_ip','export_date']],s3.primary_mac_address.combine_first(s3.secondary_mac_address)],axis=1)
-c = c.rename(columns={'poll_last_utc': 'last_pinged_at','primary_mac_address':'mac_address'})
-
-
-
-
-def clean_asset_tags(x):
+def case(x):
     if x == 'None':
         return 'P-PLA-101-NWA-01'
     else:
         return 'P-PLA-102-NWA-01'
 
-s3['sku'].fillna('None',inplace=True)
+def nullif(x):
+	if x == 'player':
+		return None
+	else:
+		return x
 
-CASE WHEN a.secondary_mac_address IS NOT NULL THEN 'P-PLA-102-NWA-01' ELSE 'P-PLA-101-NWA-01' END
+
+s3['secondary_mac_address'].fillna('None',inplace=True)
+c=pd.concat([s3[['name','display_unit_id','poll_last_utc','host_id','public_ip','export_date']],s3.primary_mac_address.combine_first(s3.secondary_mac_address)],axis=1)
+c = c.rename(columns={'poll_last_utc': 'last_pinged_at','primary_mac_address':'mac_address'})
+
+s3['sku'].fillna('None',inplace=True)
+c['sku'] = s3['sku'].apply(nullif).combine_first(s3['secondary_mac_address'].apply(case))
+
+
 
 
 
