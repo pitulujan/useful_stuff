2607d374e29ca529404dffd15041a325e557b33e
pitulujan <lujanmartin91@gmail.com>
2019-07-15 14:40:58 -0300

    take-home

--- logic.py
+++ logic.py
@@ -45,23 +45,24 @@ s2 = pd.merge(s1, c, how='left', left_on=[broadsign_hosts_history.primary_mac_ad
 s3 = pd.merge(s2, ams_broadsign_migration_devices_history, how='left', left_on=[broadsign_hosts_history['host_id'],broadsign_hosts_history['export_date']],right_on=[ams_broadsign_migration_devices_history.bs_host_id.astype('int64'),'export_date'],suffixes=('','_d'))
 
 def case(x):
-    if x == 'None':
+    if x == None:
         return 'P-PLA-101-NWA-01'
     else:
         return 'P-PLA-102-NWA-01'
 
 def nullif(x):
-	if x == 'player':
+	if x == 'Player':
 		return None
 	else:
 		return x
 
 
-s3['secondary_mac_address'].fillna('None',inplace=True)
+s3['secondary_mac_address'].fillna('None').replace(to_replace='None',value=None,inplace=True)
+
 c=pd.concat([s3[['name','display_unit_id','poll_last_utc','host_id','public_ip','export_date']],s3.primary_mac_address.combine_first(s3.secondary_mac_address)],axis=1)
 c = c.rename(columns={'poll_last_utc': 'last_pinged_at','primary_mac_address':'mac_address'})
 
-s3['sku'].fillna('None',inplace=True)
+s3['sku'].fillna('None').replace(to_replace='None',value=None,inplace=True)
 c['sku'] = s3['sku'].apply(nullif).combine_first(s3['secondary_mac_address'].apply(case))
 
 
