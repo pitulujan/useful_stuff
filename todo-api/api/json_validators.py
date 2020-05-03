3a35b147a8bcff4e1f96ee6b27a7c81575bc6c9f
Martin Lujan <lujanmartin91@gmail.com>
2020-05-03 18:28:53 -0300

    Simple Flask API implementing JWT and Blueprint for error handling, and jsonvalidator schema

--- todo-api/api/json_validators.py
+++ todo-api/api/json_validators.py
@@ -1,9 +1,6 @@
 from jsonschema import validate, Draft7Validator
 from itertools import islice
 
-
-request_json = { "done" :True, "id" : 1, "description" : "i"}
-
 update_task = Draft7Validator({ "type" : "object",
             "properties" : {
             "id": {
@@ -43,5 +40,61 @@ def iterate_properties_updatetask(request_json: dict,):
     )
     return errors
 
-pitu= iterate_properties_updatetask(request_json)
-print(pitu[0])
+
+new_task = Draft7Validator({ "type" : "object",
+            "properties" : {
+            "description": {
+                "type": "string",
+                "description": "Description of the task to be modified",
+                "minLength": 3,
+                "maxLength": 64,
+            },
+            "done": {
+                "type": "boolean",
+                "description": "Current statuts of the task",
+            },
+            "title": {
+                "type": "string",
+                "description": "Title of the task to be modified",
+                "minLength": 3,
+                "maxLength": 256,
+            },},
+            
+       
+        "required": [ "done", "title", "description"],
+    })
+
+def iterate_properties_newtask(request_json: dict,):
+    """
+    Returns a small selection of the errors encountered, or an empty list if no errors encountered
+    """
+
+    errors = []
+    
+    errors += islice(
+        new_task.iter_errors(request_json), 3
+    )
+    return errors
+
+delete_task = Draft7Validator({ "type" : "object",
+        "properties" : {
+        "id": {
+            "type": "integer",
+            "description": "Id of the task to be modified",
+        },},
+        
+    
+    "required": [ "id"],
+})
+
+def iterate_properties_deletetask(request_json: dict,):
+    """
+    Returns a small selection of the errors encountered, or an empty list if no errors encountered
+    """
+
+    errors = []
+    
+    errors += islice(
+        delete_task.iter_errors(request_json), 3
+    )
+    return errors
--- todo-api/api/routes.py
+++ todo-api/api/routes.py
@@ -6,7 +6,7 @@ from time import time
 from api import app,auth, tasks,users,private_key,public_key
 from api.models import User
 from api.errors.api_errors import NotAuthorized,JSONValidationError,IdNotFoundException
-from api.json_validators import iterate_properties_updatetask
+from api.json_validators import iterate_properties_updatetask, iterate_properties_newtask,iterate_properties_deletetask
 
 @auth.verify_password
 def verify_password(username_or_token, password):
@@ -45,9 +45,6 @@ def verify_password(username_or_token, password):
         abort(401)
 
 
-
-
-
 def verify_auth_token(token):
 
     try:
@@ -80,20 +77,25 @@ def get_tasks():
 def get_task(task_id):
     task = [task for task in tasks if task["id"] == task_id]
     if len(task) == 0:
-        abort(404)
+        raise IdNotFoundException("Id not found")
     return jsonify({"task": task[0]})
 
 
 @app.route("/todo/api/v1.0/tasks", methods=["POST"])
 @auth.login_required
 def create_task():
-    if not request.json or not "title" in request.json:
+    if not request.json:
         abort(400)
+    request_json = request.get_json(force=True)
+    parse_errors = iterate_properties_newtask(request_json)
+
+    if len(parse_errors) > 0: 
+        raise JSONValidationError(parse_errors)
     task = {
         "id": tasks[-1]["id"] + 1,
-        "title": request.json["title"],
-        "description": request.json.get("description", ""),
-        "done": False,
+        "title": request_json["title"],
+        "description" : request_json["description"],
+        "done": request_json["done"],
     }
     tasks.append(task)
     return jsonify({"task": task}), 201
@@ -114,8 +116,28 @@ def update_task():
         raise IdNotFoundException("Id not found")
 
     task_to_update[0]["done"] = request_json["done"]
+    if request_json.get('title'):
+        task_to_update[0]["title"] = request_json["title"]
+    if request_json.get('description'):
+        task_to_update[0]["description"] = request_json["description"]
 
     return jsonify({"task": task_to_update[0]})
 
+
+@app.route('/todo/api/v1.0/tasks', methods=['DELETE'])
+@auth.login_required
+def delete_task():
+
+    request_json = request.get_json(force=True)
+    parse_errors = iterate_properties_deletetask(request_json)
+
+    if len(parse_errors) > 0: 
+        raise JSONValidationError(parse_errors)
+
+    task_to_delete = [task for task in tasks if task["id"] == request_json['id']]
+    if len(task_to_delete) == 0:
+        raise IdNotFoundException("Id not found")
+    tasks.remove(task_to_delete[0])
+    return jsonify({'result': True, "task" : task_to_delete[0]})
 
 
